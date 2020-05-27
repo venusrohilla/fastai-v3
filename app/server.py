a12ef289322ee943ac4fc18b66dd43eca7cca450
Venus Rohilla <venusrohilla1@gmail.com>
2020-05-27 23:24:31 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/open?id=14ixz4uceK8tBfQGey-zPqqQ5cQ44vmuc'
 export_file_name = 'export.pkl'
 
 
